8bf770562920c9e2893fecf3f5003a4f82d53163
Mohamed Sabry <mmsabry333@gmail.com>
2024-09-23 17:47:17 +0300

    tedx french university website

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-// global variables
+window.addEventListener('load', function() {
+    var loadingScreen = document.getElementById('loading-screen');
+    loadingScreen.style.display = 'none';   
 
-// loader
-const loader = document.getElementById('preloader');
+});
 // cursor
 const cursor = document.querySelector(".cursor");
 // animation navbar
@@ -56,61 +57,3 @@ a.forEach(link => {
     })
 })
 
-
-// pre load
-
-window.addEventListener("load",() =>{
-    loader.style.display = "none";
-})
-
-// omar gabaly popup
-
-const showMore = document.getElementById('popup-btn');
-const closeBtn = document.getElementById('popup-close');
-const popup = document.getElementById('popup');
-
-showMore.addEventListener('click',()=>{
-    popup.classList.add('active')
-})
-
-closeBtn.addEventListener('click',()=> {
-    popup.classList.remove('active')
-})
-
-// moustafa yasser
-
-const showMore1 = document.getElementById('popup-btn1');
-const closeBtn1 = document.getElementById('popup-close1');
-const popup1 = document.getElementById('popup1');
-showMore1.addEventListener('click',()=>{
-    popup1.classList.add('active')
-})
-
-closeBtn1.addEventListener('click',()=> {
-    popup1.classList.remove('active')
-})
-
-
-// Essam goda
-const showMore2 = document.getElementById('popup-btn2');
-const closeBtn2 = document.getElementById('popup-close2');
-const popup2 = document.getElementById('popup2');
-showMore2.addEventListener('click',()=>{
-    popup2.classList.add('active')
-})
-
-closeBtn2.addEventListener('click',()=> {
-    popup2.classList.remove('active')
-})
-
-// Essam goda
-const showMore3 = document.getElementById('popup-btn3');
-const closeBtn3 = document.getElementById('popup-close3');
-const popup3 = document.getElementById('popup3');
-showMore3.addEventListener('click',()=>{
-    popup3.classList.add('active')
-})
-
-closeBtn3.addEventListener('click',()=> {
-    popup3.classList.remove('active')
-})
